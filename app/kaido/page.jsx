e82775edb018bbfed5ae39223e0b836702419043
Joakim Villo <villokodehode@gmail.com>
2024-05-23 11:20:04 +0200

    moved image to public folder (andreas)

--- app/kaido/page.jsx
+++ app/kaido/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import myImage from './kaido/profilBilde.jpg'; // Adjust the path according to your project's structure
+// import myImage from './kaido/profilBilde.jpg'; // Adjust the path according to your project's structure
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">I don't write typescript :P</h1>
-      <img src={myImage} alt="image"/>
+      <h1 className="text-3xl font-bold underline">I  don&apos;t write typescript :P</h1>
+      <Image width={300} height={300} src="/profilBilde.jpg" alt="image"/>
     </main>
   );
 }
