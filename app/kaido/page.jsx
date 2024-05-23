6ef7a024b1b8825c839d46d6eb801ef99a1edb41
KaidoK-B04 <131766275+KaidoK-B04@users.noreply.github.com>
2024-05-23 10:59:50 +0200

    image added

--- app/kaido/page.jsx
+++ app/kaido/page.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import myImage from './kaido/profilBilde.jpg'; // Adjust the path according to your project's structure
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline" >Test page from Kaido attempt worked well</h1>
+      <h1 className="text-3xl font-bold underline">I don't write typescript :P</h1>
+      <img src={myImage} alt="image"/>
     </main>
   );
 }
