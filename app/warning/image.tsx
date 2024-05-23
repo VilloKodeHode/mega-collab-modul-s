808283adb4c621c47b3e8e750b9cb12f51d0e465
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-05-23 13:01:41 +0200

    t

---
+++ app/warning/image.tsx
@@ -0,0 +1,13 @@
+const Image = () => {
+  return (
+    <div className="bg-white">
+      <img
+        src="/app/warning/components/images/Saturn.jpg"
+        alt=""
+        className="object-cover h-60 w-60"
+      />
+    </div>
+  );
+};
+
+export default Image;
