69bb0e5b78e00ccc7f30d488cde55d02d4c842c5
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-05-23 13:01:34 +0200

    sd

--- app/warning/page.tsx
+++ app/warning/page.tsx
@@ -1,11 +1,13 @@
 import Button from "./button";
+import Image from "./image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-medium text-gray-900 dark:text-white">
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
         Vidar was here!
       </h1>
+      <Image></Image>
       <Button></Button>
     </main>
   );
