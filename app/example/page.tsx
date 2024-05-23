92290f1e7d38653a896aa01d6421321e77384ee8
andreasb09 <andreasb09.kodehode@gmail.com>
2024-05-23 10:09:40 +0200

    changed h1 to h2 and example text

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline" >Example page</h1>
+      <h2 className="text-3xl font-bold underline" >Hey Universe</h2>
     </main>
   );
 }
