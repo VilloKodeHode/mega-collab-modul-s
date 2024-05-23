6f2b9fdf1a465d05a3c9930b15326d19a5d4882b
André <137167063+AndreK-B06@users.noreply.github.com>
2024-05-23 10:14:04 +0200

    test André

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">test test</h1>
     </main>
   );
 }
