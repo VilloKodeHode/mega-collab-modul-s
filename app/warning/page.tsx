0082b41a6c7ecfecf5c973b2e6d502d0bf7acf2e
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-05-23 12:35:13 +0200

    cc

--- app/warning/page.tsx
+++ app/warning/page.tsx
@@ -3,7 +3,7 @@ import Button from "./button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-lg font-medium text-gray-900 dark:text-white">
+      <h1 className="text-2xl font-medium text-gray-900 dark:text-white">
         Vidar was here!
       </h1>
       <Button></Button>
