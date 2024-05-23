78a9a9eabec2a3298b4980ed7659b652ba76b0a5
andreasb09 <andreasb09.kodehode@gmail.com>
2024-05-23 10:25:34 +0200

    added manaNode page

---
+++ app/manaNode/page.tsx
@@ -0,0 +1,9 @@
+export default function manaNode() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline text-amber-50">
+        Overflowing with energy!
+      </h1>
+    </main>
+  );
+}
