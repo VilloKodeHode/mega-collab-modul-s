4fcfac956834488826ccb6eadadde00270750b90
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-05-23 13:12:27 +0200

    s

--- app/warning/image.tsx
+++ app/warning/image.tsx
@@ -2,7 +2,7 @@ const Image = () => {
   return (
     <div className="bg-white">
       <img
-        src="/app/warning/components/images/Saturn.jpg"
+        src="./app/warning/components/images/Saturn.jpg"
         alt=""
         className="object-cover h-60 w-60"
       />
