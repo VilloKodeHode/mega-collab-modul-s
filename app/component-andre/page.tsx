1e7c95ae37057b3ec217b560d0f01ec286b8babc
André <137167063+AndreK-B06@users.noreply.github.com>
2024-05-29 11:30:11 +0200

    training

--- app/component-andre/page.tsx
+++ app/component-andre/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">test test</h1>
-      <h2 className="text-3xl font-bold underline">You shall not pace</h2>
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <nav className="w-full bg-blue-500">
+        <button className="px-10">home</button>
+        <button className="px-10">about</button>
+        <button className="px-10">why?</button>
+      </nav>
+      <section id="content"></section>
+      <h1 className="mt-20 text-3xl font-bold underline">Hello</h1>
+      <h2 className="mt-10 text-5xl font-bold">You shall not pace</h2>
     </main>
   );
 }
