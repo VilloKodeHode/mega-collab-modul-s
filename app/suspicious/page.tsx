8413b782bb9c86f2d3f6c546defeab06240f06aa
kristoffach <kristofferfh.kodehode@gmail.com>
2024-05-30 03:00:13 +0200

    Code refactoring, adding resizeobserver

--- app/suspicious/page.tsx
+++ app/suspicious/page.tsx
@@ -3,62 +3,56 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const [ height, setHeight ] = useState<number>(0);
-  const [ width, setWidth ] = useState<number>(0);
-
-  const containerRef = useRef<HTMLDivElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D>(null);
 
-  const getRandomBetween = (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min) ) + min;
+  const getRandomNumberBetween = (min:number, max:number):number => {
+    return Math.floor(Math.random() * ( max - min + 1 ) + min)
   }
-
-  const getRandomColor = () => {
-    const colorList = ["asd",123123,1231231241]
-    const color = colorList[getRandomBetween(0, colorList.length)]
-    return `#${color}`
-  }
-
-  const drawLine = () => {
-
-  }
-
 
   useEffect(() => {
-    // Get parent element size
-    if (containerRef.current) {
-      setHeight(containerRef.current.clientHeight)
-      setWidth(containerRef.current.clientWidth)
-    }
-
-    // Resize canvas to parrent element size
+    // Define elements and containers on mount
+    const container = containerRef.current;
     const canvas = canvasRef.current;
-    if (canvas) {
-      canvas.height = height;
-      canvas.width = width;
-    }
+    const context = canvas?.getContext('2d');
 
-    // Create context for drawing on canvas
-    const context = canvas?.getContext('2d')
-    if (!context) return
+    // Verify mounting
+    if (!container || !canvas || !context) throw new Error("Error in mounting canvas element");
 
-    const draw = setInterval(()=>{
-      context.beginPath();
-      context.moveTo(getRandomBetween(0, width), getRandomBetween(0, height));
-      context.lineTo(getRandomBetween(0, width), getRandomBetween(0, height));
-      context.stroke();
-    }, 1)
+    // Create new resize observer
+    const resizeObserver = new ResizeObserver(() => {
+      canvas.width = container.clientWidth;
+      canvas.height = container.clientHeight;
+    })
 
-    const recolor = setInterval(()=>{
+    // Attach observer to main element, resizes canvas to parent element
+    resizeObserver.observe(container);
+
+    // Select random color
+    const setColor = setInterval(() => {
       const colorList = ["03071E","370617", "6A040F", "9D0208", "D00000", "DC2F02", "E85D04", "F48C06", "FAA307", "FFBA08"]
-      context.strokeStyle = `#${colorList[getRandomBetween(0, colorList.length)]}`
-    }, 1000)
+      context.strokeStyle = `#${colorList[getRandomNumberBetween(0, colorList.length)]}`
+    }, 2000)
 
-  }, [height, width])
+    // Start animating on canvas
+    const drawLine = setInterval(() => {
+      context.beginPath();
+      context.moveTo(getRandomNumberBetween(0, canvas.width), getRandomNumberBetween(0, canvas.height));
+      context.lineTo(getRandomNumberBetween(0, canvas.width), getRandomNumberBetween(0, canvas.height));
+      context.stroke();
+    }, 10)
+
+    // Remove listeners and observers
+    return () => {
+      resizeObserver.disconnect()
+      clearInterval(drawLine)
+      clearInterval(setColor)
+    }
+  }, [])
 
   return (
-    <main ref={containerRef} className="min-h-svh">
+    <main ref={containerRef} className="flex-1">
       <canvas ref={canvasRef} ></canvas>
     </main>
   );
