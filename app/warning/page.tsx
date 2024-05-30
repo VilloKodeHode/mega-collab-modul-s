05a8781977727a11c0be7a080ce70a19965c58f7
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-05-30 12:45:56 +0200

    progress

--- app/warning/page.tsx
+++ app/warning/page.tsx
@@ -2,22 +2,18 @@ import Button from "./button";
 import Image from "./image";
 import Header from "./header";
 
-
 export default function Home() {
   return (
-<<<<<<< HEAD
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
         Vidar was here!
       </h1>
-=======
-    <main className="flex min-h-screen flex-col items-center justify-between pb-20">
       {/* <Header/> */}
       <h1 className="text-medium font-bold text-gray-900 dark:text-white">
         Vidar was here!
       </h1>
       <Image></Image>
->>>>>>> c953bb8f37f5fae5941c0d69c8dbc3cb643f9fc2
+
       <Button></Button>
     </main>
   );
