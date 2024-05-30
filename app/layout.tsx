f53d92cde509bf6cc87e6a553d6978ed974dd33e
kristoffach <kristofferfh.kodehode@gmail.com>
2024-05-30 03:10:23 +0200

    Converting display type from block to flexbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`flex flex-col min-h-svh ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
