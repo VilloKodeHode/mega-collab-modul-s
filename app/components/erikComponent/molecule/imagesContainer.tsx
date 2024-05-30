9709501cb339a56da13abd86e9f938fc147428b7
NordikE <erik.b10.kodehode@gmail.com>
2024-05-30 10:55:44 +0200

    Grumble.  This isn't working right.  Fix later pl0x

--- app/components/erikComponent/molecule/imagesContainer.tsx
+++ app/components/erikComponent/molecule/imagesContainer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ImageBox from "../atom/imageBox";
+import AFArrows from "../Images/AFArrows.jpg";
 
 const ImagesContainer: React.FC = () => {
   return (
     <div>
       <ImageBox
-        imageURL="../Images/AFArrows.jpg"
+        imageURL="AFArrows"
         altText="Antifa Arrows - No Racists Allowed!"
         width={150}
         height={150}
