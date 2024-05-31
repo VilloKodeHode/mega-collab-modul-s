150af31fa0f416af3702cb316ce3f6190fa3c0c2
KaidoK-B04 <131766275+KaidoK-B04@users.noreply.github.com>
2024-05-31 15:16:41 +0200

    Visited Jesper page

--- app/Jesper/page.tsx
+++ app/Jesper/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline" > Vidar og Eirik var her </h1>
+      <h1 className="text-3xl font-bold underline" > Vidar og Eirik var her før Kaido kom</h1>
     </main>
   );
 }
