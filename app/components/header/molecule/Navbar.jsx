029fd3137c71074fa17ea8fb1993f79800f1cd31
kristoffach <kristofferfh.kodehode@gmail.com>
2024-05-31 17:02:08 +0200

    Changing text key to title

--- app/components/header/molecule/Navbar.jsx
+++ app/components/header/molecule/Navbar.jsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
   return (
     <nav className="flex md:max-w-[80%] max-w-[60%] flex-wrap justify-end gap-6">
       {MENU_LIST.map((menu)=> (
-      <NavLinks key={menu.text} href={menu.href}>{menu.text}</NavLinks>  
+      <NavLinks key={menu.title} href={menu.href}>{menu.title}</NavLinks>
       ))}
     </nav>
   );
