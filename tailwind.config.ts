160e0559228e8c76b9b3452e2e1934bf506aa7da
kristoffach <kristofferfh.kodehode@gmail.com>
2024-05-31 15:14:43 +0200

    Creating subgroups and assigning new colors from designfile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,20 +8,72 @@ const config: Config = {
   ],
   theme: {
     extend: {
-colors: {
-  primary: '#ffb300',
-  secondary: '#f00',
-  tertiary: '#0f0',
-  black: '#181100', // 95-98% black
-  black80: '#00000080', // 80% black
-  black50: '#00000050', // 50% black
-  white: '#ffffff', // 95-98% white
-  white80: '#ffffff80', // 80% white
-  white50: '#ffffff50', // 50% white
-
-}
-    },
+      colors: {
+        // Button and interface colors
+        button: {
+          'primary': {
+            DEFAULT: '#007FE8',
+            dark: '#007FE8'
+          },
+          'secondary': {
+            DEFAULT: '#00437B',
+            dark: '#00437B'
+          },
+          'tertiary': {
+            DEFAULT: '#D2DAEE',
+            dark: '#D2DAEE'
+          },
+          'success': {
+            DEFAULT: '#6DC995',
+            dark: '#6DC995'
+          },
+          'info': {
+            DEFAULT: '#6B4897',
+            dark: '#6B4897'
+          },
+          'warning': {
+            DEFAULT: '#FF7D04',
+            dark: '#FF7D04'
+          },
+          'error': {
+            DEFAULT: '#B3001B',
+            dark: '#B3001B'
+          },
+        },
+        // Elements and backgrounds
+        element: {
+          'primary': {
+            DEFAULT: '#D2DAEE',
+            dark: '#1E3255'
+          },
+          'secondary': {
+            DEFAULT: '#3A91AA',
+            dark: '#3A91AA'
+          },
+          'tertiary': {
+            DEFAULT: '#1E3255',
+            dark: '#D2DAEE'
+          }
+        },
+        // Textboxes
+        textbox: {
+          'primary': {
+            DEFAULT: '#fff',
+            dark: '#000'
+          },
+          'secondary': {
+            DEFAULT: '#ffffff80',
+            dark: '#00000080'
+          },
+          'tertiary': {
+            DEFAULT: '#ffffff50',
+            dark: '#00000050'
+          }
+        },
+      }
+    }
   },
   plugins: [],
 };
+
 export default config;
