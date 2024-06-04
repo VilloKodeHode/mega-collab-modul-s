4e36cc16bcbfe17048fd4bd61d369b5bf51e297a
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-04 20:33:31 +0200

    Fixing broken parent tag

--- app/warning/page.tsx
+++ app/warning/page.tsx
@@ -3,13 +3,12 @@ import Pic from "../components/warning-comps/image";
 
 export default function Home() {
   return (
-
-    <main className="flex max-h-screen gap-16 flex-col items-center justify-between pt-24">
+    <div className="flex max-h-screen gap-16 flex-col items-center justify-between pt-24">
       <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
         Vidar was here!
       </h1>
       <Pic />
       <Button />
-    </>
+    </div>
   );
 }
