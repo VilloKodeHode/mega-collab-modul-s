fb248e032818f3d153c201b9e67b0e114f1f13a6
Joakim <villokodehode@gmail.com>
2024-06-05 14:31:55 +0200

    *

--- app/Erik/page.tsx
+++ app/Erik/page.tsx
@@ -4,8 +4,8 @@ import ImagesContainer from "../components/erikComponent/molecule/imagesContaine
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
       <h1 className="text-3xl font-bold underline">Erik sin side!</h1>
-    </main>
+    </>
   );
 }
