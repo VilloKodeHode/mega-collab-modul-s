0f3415e79c554ef8da8927f6b4010dd0cef680e5
Joakim <villokodehode@gmail.com>
2024-06-05 12:47:13 +0200

    made more components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-
-
-import { StdButton } from "./components/base/Button";
+import { AltButton, StdButton } from "./components/base/Button";
+import { Container } from "./components/base/Container";
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <StdButton>Click me!</StdButton>
+
+      <Container>
+        <StdButton>Click me!</StdButton>
+        <AltButton>Click me!</AltButton>
+      </Container>
     </main>
   );
 }
