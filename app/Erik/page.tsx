c2d77eb1e2c548469ace3afde95bed0ba8bb1df9
NordikE <erik.b10.kodehode@gmail.com>
2024-06-05 13:40:25 +0200

    No apostrophes in Norwegian!

--- app/Erik/page.tsx
+++ app/Erik/page.tsx
@@ -5,7 +5,7 @@ import ImagesContainer from "../components/erikComponent/molecule/imagesContaine
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">Eriks page!</h1>
+      <h1 className="text-3xl font-bold underline">Erik sin side!</h1>
       <ImagesContainer />
     </main>
   );
