0ba7568f058d99942151bbc6978b4dd3b50bf247
NordikE <erik.b10.kodehode@gmail.com>
2024-06-05 14:24:46 +0200

    Started footer, removed image container on my page

--- app/Erik/page.tsx
+++ app/Erik/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold underline">Erik sin side!</h1>
-      <ImagesContainer />
     </main>
   );
 }
