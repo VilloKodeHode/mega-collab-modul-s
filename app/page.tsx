5932b241f18fbb4b6e6ef3962169981c35a35a57
Joakim <villokodehode@gmail.com>
2024-06-05 13:00:39 +0200

    made a click util

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { AltButton, StdButton } from "./components/base/Button";
 import { Container } from "./components/base/Container";
+import { click } from "./utils/utilClicks";
 
 export default function Home() {
   return (
@@ -7,8 +8,8 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <Container>
-        <StdButton>Click me!</StdButton>
-        <AltButton>Click me!</AltButton>
+        <StdButton onClick={click}>Click me!</StdButton>
+        <AltButton onClick={click}>Click me!</AltButton>
       </Container>
     </main>
   );
