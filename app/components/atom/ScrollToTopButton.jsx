3ad90632b77f53db85353eed5873f97b3e1ed5f1
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-06-06 09:59:18 +0200

    fixed (added scrolling and dissapearing on the top)

--- app/components/atom/ScrollToTopButton.jsx
+++ app/components/atom/ScrollToTopButton.jsx
@@ -1,16 +1,47 @@
 "use client";
-
+import { animateScroll } from "react-scroll";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export function Btn() {
+  const [visible, setVisible] = useState(false);
+
+  const btnVisibility = () => {
+    if (window.scrollY > 100) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", btnVisibility);
+    return () => {
+      window.removeEventListener("scroll", btnVisibility);
+    };
+  }, []);
+
+  const options = {
+    duration: 300,
+    smooth: true,
+  };
+  const scrollingToTop = () => {
+    animateScroll.scrollToTop(options);
+  };
+
   return (
-    <button className="p-5 bg-grey-400 backdrop-filter backdrop-blur-md bg-opacity-10 rounded-full shadow-md">
-      <Image
-        src="/images/arrow.png"
-        alt="The arrow icon"
-        width={20}
-        height={20}
-      />
-    </button>
+    visible && (
+      <button
+        onClick={scrollingToTop}
+        className="p-5 bg-grey-400 backdrop-filter backdrop-blur-md bg-opacity-10 rounded-full shadow-md"
+      >
+        <Image
+          src="/images/arrow-up.svg"
+          alt="the arrow icon, button to scroll up"
+          width={35}
+          height={35}
+        />
+      </button>
+    )
   );
 }
