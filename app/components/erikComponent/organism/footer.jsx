15e486671e70189dc3a78ece9fb905c5c94ca3df
NordikE <erik.b10.kodehode@gmail.com>
2024-06-06 11:12:51 +0200

    Image sizing

--- app/components/erikComponent/organism/footer.jsx
+++ app/components/erikComponent/organism/footer.jsx
@@ -6,20 +6,22 @@ export const Footer = () => {
     { title: "Home", href: "/" },
     { title: "About", href: "/about" },
     { title: "Contact", href: "/contact" },
+    // ... Add more link items as needed
   ];
 
   return (
-    <div className="flex bg-green-800 gap-36 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
-      <div className="mx-24 mr-64">
+    <div className="flex bg-green-800 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
+      <div className="w-full h-16">
         <Image
           src="/components/images/erik/FallingStars.webp"
-          className="h-12 w-16 mt-6 ml-16"
           alt=""
-          width={150}
-          height={150}
+          layout="fill"
+          objectFit="cover"
         />
       </div>
-      <FooterLinks items={linkItems} />
+      <div className="flex-grow">
+        <FooterLinks items={linkItems} />
+      </div>
     </div>
   );
 };
