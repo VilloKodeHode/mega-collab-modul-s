4b7e48a5f7ae910b3e89bb23c9fab32a375330cd
NordikE <erik.b10.kodehode@gmail.com>
2024-06-13 09:41:50 +0200

    Updated with code (that was in the wrong place)

--- app/components/footer/organism/footer.jsx
+++ app/components/footer/organism/footer.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import { FooterLinks } from "../molecule/FooterLinks";
+
 export const Footer = () => {
+  const linkItems = [
+    { title: "Home", href: "/" },
+    { title: "About", href: "/about" },
+    { title: "Contact", href: "/contact" },
+  ];
+
   return (
-    <footer>
-      <p>this is the footer</p>
-    </footer>
+    <div className="flex bg-green-800 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
+      <div className="w-full h-16">
+        <h1 className="text-3xl font-bold">Footer!</h1>
+      </div>
+      <div className="flex-grow">
+        <FooterLinks items={linkItems} />
+      </div>
+    </div>
   );
 };
