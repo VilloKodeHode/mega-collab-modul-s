73f5f9735c896fb2862133dafa665fe118ba9915
NordikE <erik.b10.kodehode@gmail.com>
2024-06-13 12:21:04 +0200

    Header for image added

--- app/components/footer/atom/JobloopLogo.jsx
+++ app/components/footer/atom/JobloopLogo.jsx
@@ -2,13 +2,18 @@ import Image from "next/image";
 
 const JobloopLogo = () => {
   return (
-    <div className="w-[328px] h-[162px]">
-      <Image
-        src="/components/images/footer/Jobloop_Logo.png"
-        alt="Jobloop Logo"
-        width={328}
-        height={162}
-      />
+    <div className="flex flex-col">
+      <div className="imageHeader">
+        <h1>Made in assosciation with:</h1>
+      </div>
+      <div className="w-[328px] h-[162px]">
+        <Image
+          src="/components/images/footer/Jobloop_Logo.png"
+          alt="Jobloop Logo"
+          width={328}
+          height={162}
+        />
+      </div>
     </div>
   );
 };
