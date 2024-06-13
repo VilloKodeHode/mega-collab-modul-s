9c04763bf4e73fffd6776a602c4cffbaa44d8b3c
NordikE <erik.b10.kodehode@gmail.com>
2024-06-13 12:12:54 +0200

    Removed code and added image modules

--- app/components/footer/organism/footer.jsx
+++ app/components/footer/organism/footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Image from "next/image";
-import { FooterLinks } from "../molecule/FooterLinks"; // Named import
+import { FooterLinks } from "../molecule/FooterLinks";
+import DividerLeft from "./atoms/DividerLeft";
+import JobloopLogo from "./atoms/JobloopLogo";
+import DividerRight from "./atoms/DividerRight";
 
 export const Footer = () => {
   const linkItems = [
@@ -12,31 +14,9 @@ export const Footer = () => {
   return (
     <div className="flex flex-col bg-green-800 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
       <div className="container mx-auto flex justify-center items-center">
-        <div className="w-[252px] h-[323px]">
-          <Image
-            src="/components/images/footer/Footer_Divider_Left.png"
-            alt="Divider Left"
-            width={252}
-            height={323}
-          />
-        </div>
-        <div className="w-[328px] h-[162px]">
-          <h1>Made in association with:</h1>
-          <Image
-            src="/components/images/footer/Jobloop_Logo.png"
-            alt="Jobloop Logo"
-            width={328}
-            height={162}
-          />
-        </div>
-        <div className="w-[252px] h-[323px]">
-          <Image
-            src="/components/images/footer/Footer_Divider_Right.png"
-            alt="Divider Right"
-            width={252}
-            height={323}
-          />
-        </div>
+        <DividerLeft />
+        <JobloopLogo />
+        <DividerRight />
       </div>
       <div className="flex justify-center">
         <FooterLinks items={linkItems} />
