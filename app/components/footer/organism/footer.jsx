2acbc4022b9e115c2d17e8fbf700d4a7da49a89e
NordikE <erik.b10.kodehode@gmail.com>
2024-06-13 11:36:54 +0200

    Need to make modules

--- app/components/footer/organism/footer.jsx
+++ app/components/footer/organism/footer.jsx
@@ -10,18 +10,35 @@ export const Footer = () => {
   ];
 
   return (
-    <div className="flex bg-green-800 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
+    <div className="flex flex-col bg-green-800 p-12 w-full drop-shadow-[0_2px_2px_rgba(255,255,255,0.75)]">
       <div className="container mx-auto flex justify-center items-center">
-        <div className="w-[328px] h-[162px]">
+        <div className="w-[252px] h-[323px]">
           <Image
-            src="/components/images/footer/logo.png"
-            alt="Company Logo"
+            src="/components/images/footer/Footer_Divider_Left.png"
+            alt="Divider Left"
+            width={252}
+            height={323}
+          />
+        </div>
+        <div className="w-[328px] h-[162px]">
+          <h1>Made in association with:</h1>
+          <Image
+            src="/components/images/footer/Jobloop_Logo.png"
+            alt="Jobloop Logo"
             width={328}
             height={162}
           />
         </div>
+        <div className="w-[252px] h-[323px]">
+          <Image
+            src="/components/images/footer/Footer_Divider_Right.png"
+            alt="Divider Right"
+            width={252}
+            height={323}
+          />
+        </div>
       </div>
-      <div className="flex-grow">
+      <div className="flex justify-center">
         <FooterLinks items={linkItems} />
       </div>
     </div>
