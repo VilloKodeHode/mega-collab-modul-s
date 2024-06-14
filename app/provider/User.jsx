bd458a1840c102af4319d7d0e72aa4b33225f3cc
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-14 15:59:01 +0200

    Adding useEffect to userProvider

--- app/provider/User.jsx
+++ app/provider/User.jsx
@@ -1,13 +1,21 @@
 "use client"
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../context/User";
 
 const UserProvider = ({ children }) => {
-  const [ dark, toggleDark ] = useState('dark')
+  const [ darkmode, toggleDark ] = useState()
+
+  useEffect(() => {
+    // Check is user preferes darkmode
+    const prefDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Set darkmode
+    toggleDark(prefDark)
+  }, [toggleDark])
 
   return (
-    <UserContext.Provider value={{ dark, toggleDark }}>
+    <UserContext.Provider value={{ darkmode, toggleDark }}>
       { children }
     </UserContext.Provider>
   );
