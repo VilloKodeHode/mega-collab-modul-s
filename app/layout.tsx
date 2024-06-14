43b8dec6d57f9ef49bb06e37cd04f663c39212f6
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-14 17:46:35 +0200

    Adding hydration supress on html, should not cascade down

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <UserProvider>
         <body className={`flex flex-col min-h-svh ${inter.className}`}>
           <Header />
