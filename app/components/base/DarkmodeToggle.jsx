1e8c1943aad7cf2e95ec39b7a0d35a0d071a996f
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-14 15:28:31 +0200

    darkmode button now interacts with usercontext theme

--- app/components/base/DarkmodeToggle.jsx
+++ app/components/base/DarkmodeToggle.jsx
@@ -1,24 +1,20 @@
 "use client"
 
-import { useState } from "react";
+import { useContext, useState } from "react";
+import UserContext from "../../context/User";
 import { FaMoon, FaSun } from "react-icons/fa";
 
-export const DarkmodeToggle = ({icon = false}) => {
-  const [ isDark, toggleDark ] = useState()
-
-  const toggleHandler = () => {
-    // Change mode
-    toggleDark(!isDark)
-  }
+export const DarkmodeToggle = ({ icon = false }) => {
+  const { dark, toggleDark } = useContext(UserContext);
 
   return (
     <button
       aria-label="Toggle Theme Mode"
       type="button"
       className="flex items-center justify-center"
-      onClick={() => toggleHandler()}
+      onClick={() => toggleDark(!dark)}
     >
-      { icon && ( isDark && <FaSun /> || !isDark && <FaMoon /> ) || !icon && ( isDark && "Light" || !isDark && "Dark" ) }
+      { icon ? ( dark ? <FaSun /> : <FaMoon /> ) : dark ? "Light" : "Dark" }
     </button>
   )
 };
