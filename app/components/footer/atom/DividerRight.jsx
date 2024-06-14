84ddb497de6b689f37dc5d72ba1911985fc4bc8f
NordikE <erik.b10.kodehode@gmail.com>
2024-06-14 09:34:25 +0200

    Padding

--- app/components/footer/atom/DividerRight.jsx
+++ app/components/footer/atom/DividerRight.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const DividerRight = () => {
   return (
-    <div className="w-[252px] h-[323px]">
+    <div className="p-0 w-[252px] h-[323px]">
       <Image
         src="/components/images/footer/Footer_Divider_Right.png"
         alt="Divider Right"
