30ce41971487eb287bbed374528d362795c3baca
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-16 17:13:57 +0200

    Adding some tailwind styles

--- app/components/base/PageCard.tsx
+++ app/components/base/PageCard.tsx
@@ -9,14 +9,23 @@ export const PageCard = (props: Page_item) => {
     <div key={href} className="
       flex
       flex-col
-      gap-2
+      gap-4
       p-4
       rounded
 
-      bg-element-primary
-      dark:bg-element-primary-dark
+      text-black
+      dark:text-white
+r
+      bg-gradient-to-b
+      from-element-primary
+      hover:from-element-secondary
+      dark:from-element-primary-dark
+
+      transition-colors
+      ease-in-out
+      duration-200
     ">
-      <h3>{title}</h3>
+      <h3 className="text-2xl">{title}</h3>
       <div className="hidden">
         {img.length ? img.map(item =>
           <Image key={item} src={item} alt={desc} width={400} height={400}/>)
@@ -26,13 +35,13 @@ export const PageCard = (props: Page_item) => {
 
       </div>
       <span>{desc}</span>
-      <span className="flex justify-between">
+      <span className="flex justify-between mt-auto">
         <span className="flex gap-2 text-xs">
           {tags && tags.map((tag, index) =>
-            <p key={index} className="px-2 py-1 padding border-2 border-solid rounded-full">{tag}</p>
+            <p key={index} className="px-2 py-1 padding border-2 border-solid rounded-full border-blue-300 text-cyan-100">{tag}</p>
           )}
         </span>
-        <Link className="text-blue-400 " key={title} href={href}>visit</Link>
+        <Link className="text-blue-400 hover:text-blue-500" key={title} href={href}>visit</Link>
       </span>
     </div>
   );
