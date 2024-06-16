6f5c678bc107ba379d26517e71f36c62be1cc0a9
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-16 21:31:17 +0200

    Fixing hero card bg

--- app/components/base/Container.jsx
+++ app/components/base/Container.jsx
@@ -1,6 +1,6 @@
 export const Container = ({children}) => {
     return (
-        <div className="p-16 bg-contrast-100 rounded-lg m-8 flex gap-8 flex-col">
+        <div className="p-16 bg-contrast-300 dark:bg-contrast-100 rounded-lg m-8 flex gap-8 flex-col">
             {children}
         </div>
     )
