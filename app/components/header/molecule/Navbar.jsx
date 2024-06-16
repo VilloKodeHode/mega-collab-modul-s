e536344d690cddbf0224139049fb06e9723f7252
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-16 20:28:42 +0200

    Changing styles and adding new menu_list structure

--- app/components/header/molecule/Navbar.jsx
+++ app/components/header/molecule/Navbar.jsx
@@ -1,16 +1,29 @@
-import { MENU_LIST } from "../../../data/menuItems";
+import Link from "next/link";
+import MENU_LIST from "../../../data/menuItems";
 import { DarkmodeToggle } from "../../base/DarkmodeToggle";
-import { NavBarItem } from "./NavBarItem";
+import { DropDown } from "./DropDown";
 
 export const NavBar = () => {
   return (
-    <nav className="flex">
-      {MENU_LIST.map( item => (
-        <NavBarItem key={item.title} {...item} />
-      ))}
-      <div className="text-xl p-4 hover:text-red-200">
-        <DarkmodeToggle icon />
-      </div>
+    <nav className="
+      flex
+      items-center
+      rounded-lg
+      gap-4
+      px-4
+
+
+
+      text-black
+      dark:text-white
+
+      bg-element-primary
+      dark:bg-element-primary-dark
+    ">
+      <Link className="p-2" href="/about">About</Link>
+      <Link className="p-2" href="/contact">Contact</Link>
+      <DarkmodeToggle icon />
+      <DropDown items={MENU_LIST} />
     </nav>
   );
 };
