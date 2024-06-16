06d12fca7306684f7904145017f19612b012da0e
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-16 16:44:16 +0200

    Creating page hub menu array

---
+++ app/data/pageHub.js
@@ -0,0 +1,91 @@
+export const PAGE_LIST = [
+  {
+    title: "Andre",
+    href: "/component-andre",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Erik",
+    href: "/Erik",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Inna",
+    href: "/inna",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Jesper",
+    href: "/Jesper",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Kaido",
+    href: "/kaido",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [
+      "/profilBilde.jpg"
+    ],
+    tags: [],
+  },{
+    title: "ManaNode",
+    href: "/manaNode",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "MCC",
+    href: "/MegaConfusionCorp",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "RobertEx",
+    href: "/robertExample",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Supersecret",
+    href: "/supersecret",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Suspiscious",
+    href: "/suspicious",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Warning",
+    href: "/warning",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [],
+    tags: [],
+  },{
+    title: "Weather",
+    href: "/Weather",
+    desc: "Beskrivelse av innhold",
+    children: [],
+    img: [
+      "/components/images/LOOGOO.png"
+    ],
+    tags: ["Weather", "Graphical"],
+  }
+]
