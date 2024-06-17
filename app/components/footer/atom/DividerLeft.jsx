53c6e729703ddaae208c1b9b392dba477395a2a5
NordikE <erik.b10.kodehode@gmail.com>
2024-06-17 10:28:29 +0200

    Removed margin and padding

--- app/components/footer/atom/DividerLeft.jsx
+++ app/components/footer/atom/DividerLeft.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const DividerLeft = () => {
   return (
-    <div className="relative w-[252px] h-[323px]">
+    <div className="relative w-[252px] h-[323px] m-0 p-0">
       <Image
         src="/components/images/footer/Footer_Divider_Left.png"
         alt="Divider Left"
