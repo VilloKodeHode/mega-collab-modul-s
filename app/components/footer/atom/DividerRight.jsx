d4672a97356fcac62e40672eb97aa852996ba127
NordikE <erik.b10.kodehode@gmail.com>
2024-06-17 10:29:12 +0200

    Removed margin and paddign

--- app/components/footer/atom/DividerRight.jsx
+++ app/components/footer/atom/DividerRight.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const DividerRight = () => {
   return (
-    <div className="relative w-[292px] h-[323px]">
+    <div className="relative w-[292px] h-[323px] m-0 p-0">
       <Image
         src="/components/images/footer/Footer_Divider_Right.png"
         alt="Divider Right"
