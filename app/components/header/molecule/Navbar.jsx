b46f2cfa70f6ae7c27e0179bf1f54902f398a359
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-19 15:59:13 +0200

    Adding conditional link to home in menu on small screens

--- app/components/header/molecule/Navbar.jsx
+++ app/components/header/molecule/Navbar.jsx
@@ -25,9 +25,9 @@ export const NavBar = () => {
 
       shadow-lg
     ">
+      <Link className="p-2 hover:underline hover:text-element-primary-dark dark:hover:text-element-primary sm:hidden" href="/">Home</Link>
       <Link className="p-2 hover:underline hover:text-element-primary-dark dark:hover:text-element-primary" href="/about">About</Link>
       <Link className="p-2 hover:underline hover:text-element-primary-dark dark:hover:text-element-primary" href="/contact">Contact</Link>
-      <Link className="p-2 hover:underline hover:text-element-primary-dark dark:hover:text-element-primary" href="/pagehub">Pages</Link>
       <DarkmodeToggle icon />
       <DropDown items={MENU_LIST} />
     </nav>
