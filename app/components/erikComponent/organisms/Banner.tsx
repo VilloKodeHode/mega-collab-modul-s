06010e6ddd73dd5db32ca6b0a93c9006fd75c445
NordikE <erik.b10.kodehode@gmail.com>
2024-06-19 11:08:45 +0200

    Fixing paths

--- app/components/erikComponent/organisms/Banner.tsx
+++ app/components/erikComponent/organisms/Banner.tsx
@@ -5,7 +5,7 @@ const Banner: React.FC = () => {
   return (
     <Banner id="Banner">
       <ImageBox
-        src="/Images/ritesbanner.png"
+        src="/../../../public/images/pages/erik/ritesbanner.png"
         alt="Band banner with Sax, Drag and Rock'n'Roll tagline"
         width={500}
         height={150}
