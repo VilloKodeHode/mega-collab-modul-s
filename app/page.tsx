fcb648dcca97ef183cb6fbe786a9ded68bc5ed0c
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-24 16:25:21 +0200

    Importing new hero component on app home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import { AltButton, StdButton } from "./components/base/Button";
 import { Container } from "./components/base/Container";
+import { HeroContainer } from "./components/hero/Container";
 import { HeroSection } from "./components/section/HeroSection";
 import { click } from "./utils/utilClicks";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <>
+      <HeroContainer />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-<HeroSection/>
-      {/* <Container>
+      <HeroSection/>
+      <Container>
         <StdButton onClick={click}>Click me!</StdButton>
         <AltButton onClick={click}>Click me!</AltButton>
-      </Container> */}
-
-    </main>
+      </Container>
+    </>
   );
-}
+};
