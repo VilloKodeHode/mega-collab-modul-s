ecf5c1c03bd57ad059ac09dd35999dd898cd9aa0
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-24 17:42:01 +0200

    Updated hero text style

--- app/components/hero/Text.jsx
+++ app/components/hero/Text.jsx
@@ -1,7 +1,24 @@
 export const HeroText = ({children}) => {
   return (
-    <div className="flex flex-col items-center gap-8 p-8 max-w-xs rounded-lg bg-contrast-300 dark:bg-contrast-100 z-10">
-      {children}
+    <div className="
+      absolute
+      sm:left-[20svw]
+
+      flex
+      flex-col
+      items-center
+
+      gap-8
+      p-8
+
+      max-w-xs
+      max-sm:max-w-[80%]
+      rounded-lg
+
+      bg-contrast-300
+      dark:bg-contrast-100 z-10
+    ">
+      <p>{children}</p>
       <span>arrow</span>
     </div>
   )
