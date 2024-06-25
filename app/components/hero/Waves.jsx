e6235e042458dd88b9a27ed4cc20fb30a448c6f4
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-25 22:32:54 +0200

    Adding opacity handler and converting to css svg instead of svg html ele

--- app/components/hero/Waves.jsx
+++ app/components/hero/Waves.jsx
@@ -1,33 +1,30 @@
-import classes from "./Waves.module.css";
+"use client";
+
+import css from "./Waves.module.css";
+import { useState, useEffect } from "react";
 
 export const HeroWaves = () => {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    // On page load
+    setOpacity(1 - window.scrollY / window.innerHeight);
+
+    // Create bezier curve function for better opacity transition
+    const onScroll = () => setOpacity(1 - window.scrollY / window.innerHeight);
+
+    // Attach and clean event listener
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [])
+
   return (
-    <div className={`${classes.container} right-24 max-sm:right-0 z-0`}>
-      <div className={`${classes.waves} -rotate-12`}>
-      <svg
-        width="100%"
-        height="200%"
-        viewBox="0 -1000 400 2000"
-        xmlns="http://www.w3.org/4000/svg"
-        preserveAspectRatio="none"
-      >
-        <path
-          d="M 100 -1000 C 0 -500 200 -500 100 0 C 0 500 200 500 100 1000 L 400 1000 L 400 -1000 Z"
-          fill="#006D84"
-        />
-        <path
-          d="M 150 -1000 C 0 -500 300 -500 150 0 C 0 500 300 500 150 1000 L 400 1000 L 400 -1000 Z"
-          className="fill-element-secondary"
-        />
-        <path
-          d="M 200 -1000 C 0 -500 400 -500 200 0 C 0 500 400 500 200 1000 L 400 1000 L 400 -1000 Z"
-          fill="#04718D"
-        />
-        <path
-          d="M 250 -1000 C 150 -500 350 -500 250 0 C 150 500 350 500 250 1000 L 400 1000 L 400 -1000 Z"
-          className="fill-background-dark dark:fill-background"
-        />
-      </svg>
+    <div style={{ opacity: opacity }}  className="absolute right-0 h-full w-[450px]">
+      <div className="-rotate-12 origin-top-left h-full w-[200px]">
+        <span className={css.wave}></span>
+        <span className={css.wave}></span>
+        <span className={css.wave}></span>
+        <span className={css.wave}></span>
       </div>
     </div>
   );
