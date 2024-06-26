882c2006c4e4f1806b05cbeb27e00e73d56fc2bf
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-26 19:08:20 +0200

    Creating placeholder components for each subject

---
+++ app/slides/concepts/index.tsx
@@ -0,0 +1,24 @@
+export default function SlideConcepts() {
+  return (
+    <>
+      <section>
+        <section>
+          <h2>Describing cloud concepts</h2>
+        </section>
+        <section>
+          <h3>bla bla</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex earum ab minima? At voluptatibus atque, reiciendis quae ea voluptatem doloremque.</p>
+        </section>
+      </section>
+      <section>
+        <section>
+          <h2>Benefits of cloud services</h2>
+        </section>
+        <section>
+          <h3>bla bla</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex earum ab minima? At voluptatibus atque, reiciendis quae ea voluptatem doloremque.</p>
+        </section>
+      </section>
+    </>
+  );
+}
---
+++ app/slides/management/index.tsx
@@ -0,0 +1,15 @@
+export default function SlideManagement() {
+  return (
+    <section>
+    <h2>Title</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis impedit facilis, et unde itaque ad perspiciatis error non corrupti molestiae.</p>
+    <ul>
+      <li>
+        <p>test</p>
+        <p>lets double down</p>
+      </li>
+      <li>test</li>
+    </ul>
+  </section>
+  );
+}
